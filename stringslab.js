3247cf763c4e5a39da2a5ef4e9bf3c751f4728b2
StormieMurphy <stolenmurphy@gmail.com>
2025-06-03 19:54:40 -0500

    Renamed folder to "javascript labs"

--- stringslab.js
+++ stringslab.js
@@ -6,6 +6,7 @@ console.log(spacedStatement.at(2))
 console.log(spacedStatement.trim())
 console.log(spacedStatement.toLowerCase())
 
+
 // Task 2
 
 console.log("Jack and Jill went up the hill \nto fetch a pail of water. \nJack fell down and broke his crown \nand Jill came tumbling after.")
