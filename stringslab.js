b4d4254702d94d103f0de3086d31ef53adca0161
StormieMurphy <stolenmurphy@gmail.com>
2025-06-03 19:36:38 -0500

    Completed Stings Lab Assignment

---
+++ stringslab.js
@@ -0,0 +1,11 @@
+// Task 1
+
+const spacedStatement = "  This statement has empty spaces  ";
+console.log(spacedStatement.includes("emp"))
+console.log(spacedStatement.at(2))
+console.log(spacedStatement.trim())
+console.log(spacedStatement.toLowerCase())
+
+// Task 2
+
+console.log("Jack and Jill went up the hill \nto fetch a pail of water. \nJack fell down and broke his crown \nand Jill came tumbling after.")
